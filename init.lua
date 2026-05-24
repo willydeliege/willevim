07a279aea2ad9f7b4dc3c24b2e5f83c34be0b6ac
Frédéric Willem <frederic.willem@gmail.com>
2026-05-24 21:56:51 +0200

    fix typo

--- init.lua
+++ init.lua
@@ -4,4 +4,4 @@ vim.pack.add({
 	"https://github.com/miikanissi/modus-themes.nvim",
 })
 -- vim.cmd("colorscheme github_dark_high_contrast")
-vim.cmd("colorscheme modus-vivendi")
+vim.cmd("colorscheme modus_vivendi")
