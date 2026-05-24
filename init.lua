1802a900104722e9697643ea92d21e30fb9f6eec
Frédéric Willem <frederic.willem@gmail.com>
2026-05-24 21:48:35 +0200

    add modus theme

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
-vim.pack.add({ "https://github.com/projekt0n/github-nvim-theme", "https://github.com/vague-theme/vague.nvim" })
+vim.pack.add({
+	"https://github.com/projekt0n/github-nvim-theme",
+	"https://github.com/vague-theme/vague.nvim",
+	"https://github.com/miikanissi/modus-themes.nvim",
+})
 -- vim.cmd("colorscheme github_dark_high_contrast")
-vim.cmd("colorscheme vague")
+vim.cmd("colorscheme modus-vivendi")
