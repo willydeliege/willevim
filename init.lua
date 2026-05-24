3ca43817e3d2d0dd3f82745ad36261ef9370edc3
Frédéric Willem <frederic.willem@gmail.com>
2026-05-24 11:20:29 +0200

    add quixk config for theme

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 vim.pack.add({ "https://github.com/projekt0n/github-nvim-theme", "https://github.com/vague-theme/vague.nvim" })
-vim.cmd("colorscheme github_dark_high_contrast")
+-- vim.cmd("colorscheme github_dark_high_contrast")
+vim.cmd("colorscheme vague")
