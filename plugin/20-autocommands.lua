dad60158a0fc2c59e24a5cf0bf0c6a647e8fa70d
Frédéric Willem <frederic.willem@gmail.com>
2026-05-24 11:16:32 +0200

    close help with q

--- plugin/20-autocommands.lua
+++ plugin/20-autocommands.lua
@@ -50,6 +50,12 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		end
 	end,
 })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "help", "man" },
+	callback = function()
+		vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = true, silent = true })
+	end,
+})
 -- Help to the right
 vim.api.nvim_create_autocmd("BufWinEnter", {
 	pattern = "*",
