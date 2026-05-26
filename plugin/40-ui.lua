3d457333371ea4955dca5cd30b66349129bec021
Frédéric Willem <frederic.willem@gmail.com>
2026-05-26 10:11:26 +0200

    add indent-blank-line and structure the file

--- plugin/40-ui.lua
+++ plugin/40-ui.lua
@@ -3,121 +3,189 @@ vim.pack.add({
 	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/stevearc/oil.nvim",
 	"https://github.com/j-hui/fidget.nvim",
+	"https://github.com/lukas-reineke/indent-blankline.nvim",
 })
 
--- Oil setup
-require("oil").setup({
-	-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
-	-- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
-	default_file_explorer = true,
-	float = {
-		-- Padding around the floating window
-		padding = 2,
-		-- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-		max_width = 0.8,
-		max_height = 0.8,
-		border = "rounded",
-		win_options = {
-			winblend = 0,
-		},
-	},
-})
-vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
-
--- icons
-require("nvim-web-devicons").setup({
-	-- your personal icons can go here (to override)
-	-- you can specify color or cterm_color instead of specifying both of them
-	-- DevIcon will be appended to `name`
-	override = {
-		zsh = {
-			icon = "",
-			color = "#428850",
-			cterm_color = "65",
-			name = "Zsh",
-		},
-	},
-	override_by_filename = {
-		[".gitignore"] = {
-			icon = "",
-			color = "#f1502f",
-			name = "Gitignore",
-		},
-	},
-	-- same as `override` but specifically for overrides by extension
-	-- takes effect when `strict` is true
-	override_by_extension = {
-		["log"] = {
-			icon = "",
-			color = "#81e043",
-			name = "Log",
-		},
-	},
-	-- same as `override` but specifically for operating system
-	-- takes effect when `strict` is true
-	override_by_operating_system = {
-		["apple"] = {
-			icon = "",
-			color = "#A2AAAD",
-			cterm_color = "248",
-			name = "Apple",
-		},
-	},
-})
--- Lualine setup
-require("lualine").setup({
-	options = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = {
-			statusline = {},
-			winbar = {},
-		},
-		ignore_focus = {},
-		always_divide_middle = true,
-		always_show_tabline = true,
-		globalstatus = false,
-		refresh = {
-			statusline = 1000,
-			tabline = 1000,
-			winbar = 1000,
-			refresh_time = 16, -- ~60fps
-			events = {
-				"WinEnter",
-				"BufEnter",
-				"BufWritePost",
-				"SessionLoadPost",
-				"FileChangedShellPost",
-				"VimResized",
-				"Filetype",
-				"CursorMoved",
-				"CursorMovedI",
-				"ModeChanged",
+do -- Oil setup
+	require("oil").setup({
+		-- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
+		-- Set to false if you want some other plugin (e.g. netrw) to open when you edit directories.
+		default_file_explorer = true,
+		float = {
+			-- Padding around the floating window
+			padding = 2,
+			-- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+			max_width = 0.8,
+			max_height = 0.8,
+			border = "rounded",
+			win_options = {
+				winblend = 0,
 			},
 		},
-	},
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
-		lualine_z = { "location" },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	winbar = {},
-	inactive_winbar = {},
-	extensions = {},
-})
+	})
+	vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+end
+
+do -- icons
+	require("nvim-web-devicons").setup({
+		-- your personal icons can go here (to override)
+		-- you can specify color or cterm_color instead of specifying both of them
+		-- DevIcon will be appended to `name`
+		override = {
+			zsh = {
+				icon = "",
+				color = "#428850",
+				cterm_color = "65",
+				name = "Zsh",
+			},
+		},
+		override_by_filename = {
+			[".gitignore"] = {
+				icon = "",
+				color = "#f1502f",
+				name = "Gitignore",
+			},
+		},
+		-- same as `override` but specifically for overrides by extension
+		-- takes effect when `strict` is true
+		override_by_extension = {
+			["log"] = {
+				icon = "",
+				color = "#81e043",
+				name = "Log",
+			},
+		},
+		-- same as `override` but specifically for operating system
+		-- takes effect when `strict` is true
+		override_by_operating_system = {
+			["apple"] = {
+				icon = "",
+				color = "#A2AAAD",
+				cterm_color = "248",
+				name = "Apple",
+			},
+		},
+	})
+end
+
+do -- Lualine setup
+	require("lualine").setup({
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			always_show_tabline = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 1000,
+				tabline = 1000,
+				winbar = 1000,
+				refresh_time = 16, -- ~60fps
+				events = {
+					"WinEnter",
+					"BufEnter",
+					"BufWritePost",
+					"SessionLoadPost",
+					"FileChangedShellPost",
+					"VimResized",
+					"Filetype",
+					"CursorMoved",
+					"CursorMovedI",
+					"ModeChanged",
+				},
+			},
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {},
+	})
+end
 
 require("fidget").setup({})
+-- Opt-in to hooks system if using v3
+
+do --indent-blank-line setup
+	local highlight = {
+		"RainbowRed",
+		"RainbowYellow",
+		"RainbowBlue",
+		"RainbowOrange",
+		"RainbowGreen",
+		"RainbowViolet",
+		"RainbowCyan",
+	}
+
+	local hooks = require("ibl.hooks")
+	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+	end)
+
+	require("ibl").setup({
+		indent = {
+			char = "│", -- Character used for indent guides
+			tab_char = "│", -- Character for tab indents
+			-- highlight = highlight,
+			smart_indent_cap = true,
+		},
+		whitespace = {
+			highlight = "IblWhitespace",
+			remove_blankline_trail = true,
+		},
+		scope = {
+			enabled = true,
+			char = "┃", -- Thicker char for current scope
+			highlight = highlight,
+			show_start = true,
+			show_end = true,
+			injected_languages = true,
+			include = {
+				node_type = {
+					["*"] = { "*" }, -- Highlight scope for all node types
+				},
+			},
+		},
+		exclude = {
+			filetypes = {
+				"help",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+			},
+			buftypes = { "terminal", "nofile" },
+		},
+	})
+end
