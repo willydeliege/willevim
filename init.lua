0a7d21d7f1761d4b0ab491d86013d184498ada33
Frédéric Willem <frederic.willem@gmail.com>
2026-05-26 18:55:05 +0200

    clean themes

--- init.lua
+++ init.lua
@@ -1,6 +1,4 @@
 vim.pack.add({
-	"https://github.com/projekt0n/github-nvim-theme",
-	"https://github.com/vague-theme/vague.nvim",
 	"https://github.com/miikanissi/modus-themes.nvim",
 })
 -- vim.cmd("colorscheme github_dark_high_contrast")
