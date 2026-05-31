28408607ba72fd108833594119bb2b8ff552048a
Frédéric Willem <frederic.willem@gmail.com>
2026-05-31 07:18:01 +0200

    not needed

--- plugin/35-picker.lua
+++ plugin/35-picker.lua
@@ -529,24 +529,6 @@ wk.add({
 		desc = "Prev Reference",
 		mode = { "n", "t" },
 	},
-	{
-		"<leader>N",
-		desc = "Neovim News",
-		function()
-			Snacks.win({
-				file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-				width = 0.6,
-				height = 0.6,
-				wo = {
-					spell = false,
-					wrap = false,
-					signcolumn = "yes",
-					statuscolumn = " ",
-					conceallevel = 3,
-				},
-			})
-		end,
-	},
 })
 Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
 Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
