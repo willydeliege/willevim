9bf85bcfcd47902032da2006b32236ad8db146a4
Frédéric Willem <frederic.willem@gmail.com>
2026-06-03 08:51:25 +0200

    refactor keymaps

--- plugin/35-picker.lua
+++ plugin/35-picker.lua
@@ -75,86 +75,32 @@ wk.add({
 	{ "<leader>j", group = "Jumps" },
 	{ "<leader>h", group = "Help" },
 	{ "<leader>r", group = "Resume/History" },
-	{ "<leader>q", group = "Quickfix" },
-	{ "<leader>u", group = "UI" },
+	{ "<leader>u", group = "UI/Toggle" },
 
 	----------------------------------------------------------------------------
 	-- FILES
 	----------------------------------------------------------------------------
-	{
-		"<leader>ff",
-		function()
-			fzf.files()
-		end,
-		desc = "Find Files",
-	},
-	{
-		"<leader>fg",
-		function()
-			fzf.git_files()
-		end,
-		desc = "Git Files",
-	},
-	{
-		"<leader>fr",
-		function()
-			fzf.oldfiles()
-		end,
-		desc = "Recent Files",
-	},
-	-- {
-	-- 	"<leader>fb",
-	-- 	function()
-	-- 		fzf.file_browser()
-	-- 	end,
-	-- 	desc = "File Browser",
-	-- },
+	{ "<leader>ff", fzf.files, desc = "Find Files" },
+	{ "<leader>fg", fzf.git_files, desc = "Git Files" },
+	{ "<leader>fr", fzf.oldfiles, desc = "Recent Files" },
 
 	----------------------------------------------------------------------------
 	-- SEARCH
 	---------------------------------------------------------------------------
-	{
-		"<leader>sg",
-		function()
-			fzf.live_grep()
-		end,
-		desc = "Live Grep",
-	},
-	{
-		"<leader>sw",
-		function()
-			fzf.grep_cword()
-		end,
-		desc = "Word Under Cursor",
-	},
-	{
-		"<leader>sW",
-		function()
-			fzf.grep_cWORD()
-		end,
-		desc = "WORD Under Cursor",
-	},
+	{ "<leader>sg", fzf.live_grep, desc = "Live Grep" },
+	{ "<leader>sw", fzf.grep_cword, desc = "Word Under Cursor" },
+	{ "<leader>sW", fzf.grep_cWORD, desc = "WORD Under Cursor" },
 	{
 		"<leader>sb",
 		function()
-			fzf.blines()
+			require("fzf-lua").lines({
+				profile = "ivy",
+			})
 		end,
 		desc = "Buffer Lines",
 	},
-	{
-		"<leader>sL",
-		function()
-			fzf.lines()
-		end,
-		desc = "All Buffer Lines",
-	},
-	{
-		"<leader>sr",
-		function()
-			fzf.resume()
-		end,
-		desc = "Resume Search",
-	},
+	{ "<leader>sL", fzf.lines, desc = "All Buffer Lines" },
+	{ "<leader>sr", fzf.resume, desc = "Resume Search" },
 
 	----------------------------------------------------------------------------
 	-- BUFFERS
@@ -162,338 +108,86 @@ wk.add({
 	{
 		"<leader>bb",
 		function()
-			fzf.buffers()
+			fzf.buffers({ ignore_current_buffer = true })
 		end,
 		desc = "Buffers",
 	},
-	{
-		"<leader>bm",
-		function()
-			fzf.marks()
-		end,
-		desc = "Marks",
-	},
+	{ "<leader>bm", fzf.marks, desc = "Marks" },
 
 	----------------------------------------------------------------------------
 	-- GIT
 	----------------------------------------------------------------------------
-	{
-		"<leader>gs",
-		function()
-			fzf.git_status()
-		end,
-		desc = "Status",
-	},
-	{
-		"<leader>gb",
-		function()
-			fzf.git_branches()
-		end,
-		desc = "Branches",
-	},
-	{
-		"<leader>gc",
-		function()
-			fzf.git_commits()
-		end,
-		desc = "Commits",
-	},
-	{
-		"<leader>gC",
-		function()
-			fzf.git_bcommits()
-		end,
-		desc = "Buffer Commits",
-	},
-	{
-		"<leader>gt",
-		function()
-			fzf.git_stash()
-		end,
-		desc = "Stash",
-	},
+	{ "<leader>gs", fzf.git_status, desc = "Status" },
+	{ "<leader>gb", fzf.git_branches, desc = "Branches" },
+	{ "<leader>gc", fzf.git_commits, desc = "Commits" },
+	{ "<leader>gC", fzf.git_bcommits, desc = "Buffer Commits" },
+	{ "<leader>gt", fzf.git_stash, desc = "Stash" },
 
 	----------------------------------------------------------------------------
 	-- LSP
 	----------------------------------------------------------------------------
-	{
-		"<leader>ld",
-		function()
-			fzf.lsp_definitions()
-		end,
-		desc = "Definitions",
-	},
-	{
-		"<leader>lD",
-		function()
-			fzf.lsp_declarations()
-		end,
-		desc = "Declarations",
-	},
-	{
-		"<leader>lr",
-		function()
-			fzf.lsp_references()
-		end,
-		desc = "References",
-	},
-	{
-		"<leader>li",
-		function()
-			fzf.lsp_implementations()
-		end,
-		desc = "Implementations",
-	},
-	{
-		"<leader>lt",
-		function()
-			fzf.lsp_typedefs()
-		end,
-		desc = "Type Definitions",
-	},
-	{
-		"<leader>ls",
-		function()
-			fzf.lsp_document_symbols()
-		end,
-		desc = "Document Symbols",
-	},
-	{
-		"<leader>lS",
-		function()
-			fzf.lsp_workspace_symbols()
-		end,
-		desc = "Workspace Symbols",
-	},
-	{
-		"<leader>la",
-		function()
-			vim.lsp.buf.code_action()
-		end,
-		desc = "Code Actions",
-	},
-	{
-		"<leader>ln",
-		function()
-			vim.lsp.buf.rename()
-		end,
-		desc = "Rename",
-	},
-	{
-		"<leader>lc",
-		function()
-			fzf.lsp_incoming_calls()
-		end,
-		desc = "Incoming Calls",
-	},
-	{
-		"<leader>lC",
-		function()
-			fzf.lsp_outgoing_calls()
-		end,
-		desc = "Outgoing Calls",
-	},
+	{ "<leader>ld", fzf.lsp_definitions, desc = "Definitions" },
+	{ "<leader>lD", fzf.lsp_declarations, desc = "Declarations" },
+	{ "<leader>lr", fzf.lsp_references, desc = "References" },
+	{ "<leader>li", fzf.lsp_implementations, desc = "Implementations" },
+	{ "<leader>lt", fzf.lsp_typedefs, desc = "Type Definitions" },
+	{ "<leader>ls", fzf.lsp_document_symbols, desc = "Document Symbols" },
+	{ "<leader>lS", fzf.lsp_workspace_symbols, desc = "Workspace Symbols" },
+	{ "<leader>la", vim.lsp.buf.code_action, desc = "Code Actions" },
+	{ "<leader>ln", vim.lsp.buf.rename, desc = "Rename" },
+	{ "<leader>lc", fzf.lsp_incoming_calls, desc = "Incoming Calls" },
+	{ "<leader>lC", fzf.lsp_outgoing_calls, desc = "Outgoing Calls" },
 
 	----------------------------------------------------------------------------
 	-- DIAGNOSTICS
 	----------------------------------------------------------------------------
-	{
-		"<leader>dd",
-		function()
-			fzf.diagnostics_document()
-		end,
-		desc = "Document Diagnostics",
-	},
-	{
-		"<leader>dw",
-		function()
-			fzf.diagnostics_workspace()
-		end,
-		desc = "Workspace Diagnostics",
-	},
+	{ "<leader>dd", fzf.diagnostics_document, desc = "Document Diagnostics" },
+	{ "<leader>dw", fzf.diagnostics_workspace, desc = "Workspace Diagnostics" },
 
 	----------------------------------------------------------------------------
 	-- TREESITTER / TAGS
 	----------------------------------------------------------------------------
-	{
-		"<leader>ts",
-		function()
-			fzf.treesitter()
-		end,
-		desc = "Treesitter Symbols",
-	},
-	{
-		"<leader>tt",
-		function()
-			fzf.tags()
-		end,
-		desc = "Project Tags",
-	},
-	{
-		"<leader>tb",
-		function()
-			fzf.btags()
-		end,
-		desc = "Buffer Tags",
-	},
+	{ "<leader>ts", fzf.treesitter, desc = "Treesitter Symbols" },
+	{ "<leader>tt", fzf.tags, desc = "Project Tags" },
+	{ "<leader>tb", fzf.btags, desc = "Buffer Tags" },
 
 	----------------------------------------------------------------------------
 	-- JUMPS
 	----------------------------------------------------------------------------
-	{
-		"<leader>jj",
-		function()
-			fzf.jumps()
-		end,
-		desc = "Jumps",
-	},
-	{
-		"<leader>jc",
-		function()
-			fzf.changes()
-		end,
-		desc = "Changes",
-	},
+	{ "<leader>jj", fzf.jumps, desc = "Jumps" },
+	{ "<leader>jc", fzf.changes, desc = "Changes" },
 
 	----------------------------------------------------------------------------
 	-- HELP
 	----------------------------------------------------------------------------
-	{
-		"<leader>hh",
-		function()
-			fzf.help_tags()
-		end,
-		desc = "Help Tags",
-	},
-	{
-		"<leader>hk",
-		function()
-			fzf.keymaps()
-		end,
-		desc = "Keymaps",
-	},
-	{
-		"<leader>hc",
-		function()
-			fzf.commands()
-		end,
-		desc = "Commands",
-	},
-	{
-		"<leader>ha",
-		function()
-			fzf.autocmds()
-		end,
-		desc = "Autocommands",
-	},
-	{
-		"<leader>hl",
-		function()
-			fzf.highlights()
-		end,
-		desc = "Highlights",
-	},
+	{ "<leader>hh", fzf.help_tags, desc = "Help Tags" },
+	{ "<leader>hk", fzf.keymaps, desc = "Keymaps" },
+	{ "<leader>hc", fzf.commands, desc = "Commands" },
+	{ "<leader>ha", fzf.autocmds, desc = "Autocommands" },
+	{ "<leader>hl", fzf.highlights, desc = "Highlights" },
 
 	----------------------------------------------------------------------------
 	-- HISTORY / RESUME
 	----------------------------------------------------------------------------
-	{
-		"<leader>rr",
-		function()
-			fzf.resume()
-		end,
-		desc = "Resume",
-	},
-	{
-		"<leader>rg",
-		function()
-			fzf.registers()
-		end,
-		desc = "Registers",
-	},
-	{
-		"<leader>rc",
-		function()
-			fzf.command_history()
-		end,
-		desc = "Command History",
-	},
-	{
-		"<leader>rs",
-		function()
-			fzf.search_history()
-		end,
-		desc = "Search History",
-	},
+	{ "<leader>rr", fzf.resume, desc = "Resume" },
+	{ "<leader>rg", fzf.registers, desc = "Registers" },
+	{ "<leader>rc", fzf.command_history, desc = "Command History" },
+	{ "<leader>rs", fzf.search_history, desc = "Search History" },
 
 	----------------------------------------------------------------------------
 	-- QUICKFIX
 	----------------------------------------------------------------------------
-	{
-		"<leader>sq",
-		function()
-			fzf.quickfix()
-		end,
-		desc = "Quickfix",
-	},
-	{
-		"<leader>sl",
-		function()
-			fzf.loclist()
-		end,
-		desc = "Location List",
-	},
+	{ "<leader>sq", fzf.quickfix, desc = "Quickfix" },
+	{ "<leader>sl", fzf.loclist, desc = "Location List" },
 
 	----------------------------------------------------------------------------
 	-- UI
 	----------------------------------------------------------------------------
-	{
-		"<leader>uc",
-		function()
-			fzf.colorschemes()
-		end,
-		desc = "Colorschemes",
-	},
-	{
-		"<leader>uz",
-		function()
-			fzf.spell_suggest()
-		end,
-		desc = "Spell Suggestions",
-		{
-			"<leader>/",
-			function()
-				fzf.live_grep()
-			end,
-			desc = "Grep Project",
-		},
-		{
-			"<leader><leader>",
-			function()
-				fzf.files()
-			end,
-			desc = "Find Files",
-		},
-		{
-			"gd",
-			function()
-				fzf.lsp_definitions()
-			end,
-			desc = "Goto Definition",
-		},
-		{
-			"gr",
-			function()
-				fzf.lsp_references()
-			end,
-			desc = "Goto References",
-		},
-		{
-			"gi",
-			function()
-				fzf.lsp_implementations()
-			end,
-			desc = "Goto Implementation",
-		},
-	},
+	{ "<leader>uc", fzf.colorschemes, desc = "Colorschemes" },
+	{ "<leader>uz", fzf.spell_suggest, desc = "Spell Suggestions" },
+	{ "<leader>/", fzf.live_grep, desc = "Grep Project" },
+	{ "<leader><leader>", fzf.files, desc = "Find Files" },
+	{ "gd", fzf.lsp_definitions, desc = "Goto Definition" },
+	{ "gr", fzf.lsp_references, desc = "Goto References" },
+	{ "gi", fzf.lsp_implementations, desc = "Goto Implementation" },
 })
