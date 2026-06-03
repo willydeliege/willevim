1eab14d9f8b7365e9694391cd11531fc985cdf5f
Frédéric Willem <frederic.willem@gmail.com>
2026-06-03 22:12:20 +0200

    use catppuccin theme

--- init.lua
+++ init.lua
@@ -1,5 +1,2 @@
-vim.pack.add({
-	"https://github.com/miikanissi/modus-themes.nvim",
-})
 -- vim.cmd("colorscheme github_dark_high_contrast")
-vim.cmd("colorscheme modus_vivendi")
+vim.cmd("colorscheme catppuccin")
